6f160e99331b3baca51ed775754c0a22df858b5b
Bimar Dwi Anggoro <bimarax@gmail.com>
2021-07-14 14:44:41 +0800

    -Fixed a few minor bugs

--- tic_tac_toe.py
+++ tic_tac_toe.py
@@ -1,10 +1,3 @@
-letter_position = [" " for x in range(10)]
-position = [
-    [1, 2, 3],
-    [4, 5, 6],
-    [7, 8, 9]
-]
-
 
 def board():
     print(f"   |   |   ")
@@ -32,41 +25,55 @@ def player_move(n):
     move = int(input("Choose your tile (1-9): "))
     letter_position[move] = n
 
-    column = int((move - 1) / 3)
-    row = int((move - 1) % 3)
-    position[column][row] = n
+    row = int((move - 1) / 3)
+    column = int((move - 1) % 3)
+    position[row][column] = n
 
 
 def game_status():
     # Check columns
     for i in position:
         if i[0] == i[1] and i[1] == i[2]:
-            return False
+            return 1
     # Check row
     for i in range(3):
         if position[0][i] == position[1][i] and position[1][i] == position[2][i]:
-            return False
+            return 1
     # Check diagonal
     if position[0][0] == position[1][1] and position[1][1] == position[2][2]:
-        return False
+        return 1
     if position[0][2] == position[1][1] and position[1][1] == position[2][0]:
-        return False
-    return True
+        return 1
+    # Check draw
+    if letter_position.count(" ") == 1:
+        return 0
+    return -1
 
 
 def main():
-    while game_status():
-        board()
-        player_move(letter())
     board()
-    if letter() == "O":
-        winner = "X"
-    else:
-        winner = "O"
-    print("Congrats! " + winner + " wins!")
+    player_move(letter())
+    if game_status() == -1:
+        return main()
+    if game_status() == 1:
+        board()
+        if letter() == "O":
+            winner = "X"
+        else:
+            winner = "O"
+        print("Congrats! " + winner + " wins!")
+    if game_status() == 0:
+        board()
+        print("It's a draw.")
 
 
 while True:
+    letter_position = [" " for x in range(10)]
+    position = [
+        [1, 2, 3],
+        [4, 5, 6],
+        [7, 8, 9]
+    ]
     a = input("Do you want to play? y or n: ")
     if a == "y":
         main()
